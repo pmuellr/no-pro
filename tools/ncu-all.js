929df85ecd88296faf56ff153ce167786607e778
Patrick Mueller <pmuellr@nodesource.com>
2019-02-28 18:29:12 -0500

    add support for npm-check-updates

---
+++ tools/ncu-all.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const ncuCommand = 'ncu -u --packageFile package.json'
+
+const shell = require('shelljs')
+
+const runAll = require('./run-all')
+const createDeferred = require('./lib/deferred')
+
+async function ncu () {
+  const deferred = createDeferred()
+  shell.exec(ncuCommand, (code, stdout, stderr) => {
+    deferred.resolve({ code, stdout, stderr })
+  })
+
+  return deferred.promise
+}
+
+async function main () {
+  console.log('in root...')
+  await ncu()
+  console.log('')
+
+  await runAll(ncuCommand)
+}
+
+if (require.main === module) main()
